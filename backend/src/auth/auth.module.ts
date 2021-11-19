01bd17c0e027c4808ffc2dee5ee78e075daf5fba
Ross <infiniterossi@gmail.com>
2021-11-19 08:13:31 +0000

    Add password hashing using bcrypt

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -14,6 +14,7 @@ import { AuthService } from './services/auth.service';
             })
         })
     ],
-    providers: [AuthService]
+    providers: [AuthService],
+    exports: [AuthService]
 })
 export class AuthModule {}
--- backend/src/auth/services/auth.service.ts
+++ backend/src/auth/services/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { of } from 'rxjs';
+import { from, Observable } from 'rxjs';
+import { User } from 'src/user/models/user.interface';
+
+const bcrypt = require('bcrypt');
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private readonly jwtService: JwtService){}
+
+    generateJWT(user: User): Observable<string> {
+        return from(this.jwtService.signAsync({user}));
+    }
+
+    hashPassword(password: string): Observable<string> {
+        return from<string>(bcrypt.hash(password, 12))
+    }
+
+    comparePasswords(newPassword: string, passwordHash: string): Observable<any> {
+        return of<any | boolean>(bcrypt.compare(newPassword, passwordHash));
+    }
+
+}
