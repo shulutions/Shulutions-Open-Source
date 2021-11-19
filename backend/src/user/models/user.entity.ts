1523384aa6ef0c778b78708709f06e2b81150f82
Ross <infiniterossi@gmail.com>
2021-11-19 07:43:14 +0000

    Create user interface and entity

---
+++ backend/src/user/models/user.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserEntity {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column( {unique: true} )
+    username: string;
+}
