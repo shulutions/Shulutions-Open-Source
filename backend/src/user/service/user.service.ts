6adacbe5c437d27fd2a7f1bd8d6c361936ba20d7
Ross <infiniterossi@gmail.com>
2021-11-19 08:33:25 +0000

    Add login and validate functionality to service & update existing functions

--- backend/src/user/service/user.service.ts
+++ backend/src/user/service/user.service.ts
@@ -35,11 +35,21 @@ export class UserService {
     }
 
     findOne(id: number): Observable<User> {
-        return from(this.userRepository.findOne({id})); 
+        return from(this.userRepository.findOne({id})).pipe(
+            map((user: User) => {
+                const {password, ...result} = user;
+                return result
+            })
+        )
     }
 
     findAll(): Observable<User[]> {
-        return from(this.userRepository.find());
+        return from(this.userRepository.find()).pipe(
+            map((users: User[]) => {
+                users.forEach(function (u) {delete u.password})
+                return users
+            })
+        )
     }
 
     deleteOne(id: number): Observable<any> {
@@ -47,7 +57,40 @@ export class UserService {
     }
 
     updateOne(id: number, user: User): Observable<any> {
+        delete user.email;
+        delete user.password;
         return from(this.userRepository.update(id, user));
     }
+
+    login(user: User): Observable<string> {
+        return this.validateUser(user.email, user.password).pipe (
+            switchMap((user: User) => {
+                if(user) {
+                    return this.authService.generateJWT(user).pipe(map((jwt: string) => jwt));
+                } else {
+                    return 'Wrong credentials';
+                }
+            })
+        )
+    }
+
+    validateUser(email: string, password: string): Observable<User> {
+        return this.findByEmail(email).pipe(
+            switchMap((user: User) => this.authService.comparePasswords(password, user.password).pipe(
+                map((match: boolean) => {
+                    if(match) {
+                        const {password, ...result} = user;
+                        return result;
+                    } else {
+                        throw Error;
+                    }
+                })
+            ))
+        )
+    }
+
+    findByEmail(email: string): Observable<User> {
+        return from(this.userRepository.findOne({email}));
+    }  
 
 }
