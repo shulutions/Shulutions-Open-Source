4fe4880da0cf5daefcaea28d57208c54d721714d
Ross <infiniterossi@gmail.com>
2021-11-19 07:53:16 +0000

    Add functionality to user controller

--- backend/src/user/controller/user.controller.ts
+++ backend/src/user/controller/user.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
+import { UserService } from '../service/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private userService: UserService) { }
+
+    @Post()
+    create(@Body()user: User): Observable<User> {
+        return this.userService.create(user);
+    }
+
+    @Get(':id')
+    findOne(@Param()params): Observable<User> {
+        return this.userService.findOne(params.id);
+    }
+
+    @Get()
+    findAll(): Observable<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id')id: string): Observable<User> {
+        return this.userService.deleteOne(Number(id));
+    }
+
+    @Put(':id')
+    updateOne(@Param('id') id: string, @Body() user: User): Observable<User> {
+        return this.userService.updateOne(Number(id), user);
+    }
+}
