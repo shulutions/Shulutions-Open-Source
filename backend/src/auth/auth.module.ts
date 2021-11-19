e85e03ff1792c676aaae939e619b1d3ddcdd595d
Ross <infiniterossi@gmail.com>
2021-11-19 08:05:31 +0000

    Generate auth service

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { AuthService } from './services/auth.service';
 
 @Module({
     imports: [
@@ -12,6 +13,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
                 signOptions: {expiresIn: '100s'}
             })
         })
-    ]
+    ],
+    providers: [AuthService]
 })
 export class AuthModule {}
