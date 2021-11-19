7f2c0cfc9f13cf4bfcccbe588168d05afa6e2444
Ross <infiniterossi@gmail.com>
2021-11-19 07:37:59 +0000

    Generate user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
-    ProjectModule
+    ProjectModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
