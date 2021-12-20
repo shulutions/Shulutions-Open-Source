e07d9eeb84514437264c919cdb75c98acd3f85c9
Ross Feeley <infiniterossi@gmail.com>
2021-12-20 11:07:32 +0000

    Add custom validators for registration form

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,8 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormGroupDirective, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { AuthentificationService } from 'src/app/services/authentification.service';
+
+class CustomValidators {
+  static passwordContainsNumber(control: AbstractControl): ValidationErrors | null {
+    const regex = /\d/;
+    if (regex.test(control.value) && control.value !== null) {
+      return null;
+    } else {
+      return {passwordInvalid: true}
+    }
+  }
+
+  static passwordsMatch(control: AbstractControl): ValidationErrors | null {
+      const password = control.get('password')?.value;
+      const passwordConfirm = control.get('passwordConfirm')?.value;
+
+      if((password === passwordConfirm) && password !== null && passwordConfirm !== null) {
+        return null;
+      } else {
+        return {passwordsNotMatching: true};
+      }
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -12,24 +34,28 @@ import { AuthentificationService } from 'src/app/services/authentification.servi
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup = new FormGroup({
-    name: new FormControl(null, [Validators.required]),
-    username: new FormControl(null, [Validators.required]),
-    email: new FormControl(null, [
-      Validators.required,
-      Validators.email
-    ]),
-    password: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(1) //make this bigger
-    ]),
-    passwordConfirm: new FormControl(null, [Validators.required])
-  })
+      name: new FormControl(null, [Validators.required]),
+      username: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1), //make this bigger
+        CustomValidators.passwordContainsNumber
+      ]),
+      passwordConfirm: new FormControl(null, [Validators.required])
+    }, {
+      validators: CustomValidators.passwordsMatch
+    }
+  )
 
   constructor(
     private authService: AuthentificationService,
     private formBuilder: FormBuilder,
     private router: Router
-    ) { }
+  ) { }
 
   ngOnInit(): void {
   }
