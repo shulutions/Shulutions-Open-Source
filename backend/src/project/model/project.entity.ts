4e0aa412cd6759998ac88525f39cddb4013da1b8
Ross Feeley <infiniterossi@gmail.com>
2023-02-13 14:22:59 +0000

    Add new attributes to project entity

--- backend/src/project/model/project.entity.ts
+++ backend/src/project/model/project.entity.ts
@@ -1,6 +1,7 @@
 import { UserEntity } from 'src/user/model/user.entity';
 import { ManyToOne } from 'typeorm';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ProjectStage } from './project.interface';
 
 @Entity()
 export class ProjectEntity {
@@ -28,6 +29,18 @@ export class ProjectEntity {
 
     @Column({nullable: true})
     isActive: boolean;
+
+    @Column({default: ProjectStage.PROPOSAL})
+    stage: ProjectStage;
+
+    @Column({nullable: true})
+    githubLink: string;
+
+    @Column({nullable: true})
+    discordLink: string;
+
+    @Column({nullable: true})
+    figmaLink: string;
 
     @ManyToOne(type => UserEntity, user => user.projectsManaging)
     projectManager: UserEntity
