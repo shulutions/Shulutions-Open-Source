25c81d7e9d8484c4fd7acf953454471539fa5e7d
Ross Feeley <infiniterossi@gmail.com>
2023-02-17 11:21:52 +0000

    Fix issue with discord iFrame not loading

--- frontend/src/app/pages/view-project/view-project.component.ts
+++ frontend/src/app/pages/view-project/view-project.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { ProjectService } from 'src/app/services/project-service/project.service';
 import { Project } from 'src/app/models/project.interface';
 import { map, switchMap } from 'rxjs/operators';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-view-project',
@@ -14,14 +15,24 @@ import { map, switchMap } from 'rxjs/operators';
 export class ViewProjectComponent implements OnInit {
 
   project?: Project;
+  discordLink?: SafeResourceUrl;
 
-  constructor(private activatedRoute: ActivatedRoute, private projectService: ProjectService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private projectService: ProjectService,
+    private sanitizer: DomSanitizer
+    ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       const projectId: number = parseInt(params['id']);
       this.projectService.findOne(projectId).pipe(
-        map((project: Project) => this.project = project)
+        map((project: Project) => {
+          this.project = project;
+          if (project.discordLink) {
+            this.discordLink = this.sanitizer.bypassSecurityTrustResourceUrl(project.discordLink);      
+          }
+        })
       ).subscribe();
     })
   }
