58df4fa752ec996f6e7f5a8899dff028d095fa41
Ross Feeley <infiniterossi@gmail.com>
2023-03-02 18:33:19 +0000

    Add manage project request component to router

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../guards/auth.guard';
-import { ProjectRequestComponent } from '../pages/project-request/project-request.component';
+import { ManageProjectRequestComponent } from './components/manage-project-request/manage-project-request.component';
 import { EditProjectComponent } from './pages/edit-project/edit-project.component';
 import { OverviewComponent } from './pages/overview/overview.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: OverviewComponent },
   { path: 'edit-project/:id', component: EditProjectComponent, canActivate: [AuthGuard], data: { roles: ['admin'] }},
-  { path: 'project-request/:id', component: ProjectRequestComponent, canActivate: [AuthGuard], data: { roles: ['admin'] }},
+  { path: 'project-request/:id', component: ManageProjectRequestComponent, canActivate: [AuthGuard], data: { roles: ['admin'] }},
   
 ];
 
