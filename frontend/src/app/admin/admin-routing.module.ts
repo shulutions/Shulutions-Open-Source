d6e44396a4dd0931b6866e0f971232772eaa006a
Ross Feeley <infiniterossi@gmail.com>
2023-03-03 12:09:20 +0000

    Move create project button from navigation bar to admin dashboard

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -4,11 +4,17 @@ import { AuthGuard } from '../guards/auth.guard';
 import { ManageProjectRequestComponent } from './pages/manage-project-request/manage-project-request.component';
 import { EditProjectComponent } from './pages/edit-project/edit-project.component';
 import { OverviewComponent } from './pages/overview/overview.component';
+import { CreateProjectComponent } from '../pages/create-project/create-project.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: OverviewComponent },
   { path: 'edit-project/:id', component: EditProjectComponent, canActivate: [AuthGuard], data: { roles: ['admin'] }},
   { path: 'project-request/:id', component: ManageProjectRequestComponent, canActivate: [AuthGuard], data: { roles: ['admin'] }},
+  { 
+    path: 'create-project', component: CreateProjectComponent, 
+    canActivate: [AuthGuard],
+    data: { roles: ['admin'] } 
+  }
   
 ];
 
