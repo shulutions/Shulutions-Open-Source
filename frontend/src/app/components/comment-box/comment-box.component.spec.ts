0e853d36fbb7de5a2b66d1caa20b353426b84dae
Ross Feeley <infiniterossi@gmail.com>
2023-04-21 18:31:10 +0100

    Add unit tests for comment-box and navigation components

--- frontend/src/app/components/comment-box/comment-box.component.spec.ts
+++ frontend/src/app/components/comment-box/comment-box.component.spec.ts
@@ -1,25 +1,87 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { ProjectRequestService } from 'src/app/services/project-request-service/project-request.service';
 import { CommentBoxComponent } from './comment-box.component';
 
-describe('CommentBoxComponent', () => {
+fdescribe('CommentBoxComponent', () => {
   let component: CommentBoxComponent;
   let fixture: ComponentFixture<CommentBoxComponent>;
+  let projectRequestService: jasmine.SpyObj<ProjectRequestService>;
 
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('ProjectRequestService', ['comment']);
+
     await TestBed.configureTestingModule({
-      declarations: [ CommentBoxComponent ]
-    })
-    .compileComponents();
+      declarations: [CommentBoxComponent],
+      imports: [ReactiveFormsModule],
+      providers: [{ provide: ProjectRequestService, useValue: spy }],
+    }).compileComponents();
+
+    projectRequestService = TestBed.inject(
+      ProjectRequestService
+    ) as jasmine.SpyObj<ProjectRequestService>;
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CommentBoxComponent);
     component = fixture.componentInstance;
+    component.projectRequestId = 1;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit onComment when onSubmit is called', async () => {
+    const comment = 'Test comment';
+    const emittedComment = { id: 1, comment };
+    projectRequestService.comment.and.returnValue(of(emittedComment));
+
+    component.commentForm.get('comment')?.setValue(comment);
+    fixture.detectChanges();
+
+    spyOn(component.onComment, 'emit');
+
+    await component.onSubmit();
+
+    expect(component.onComment.emit).toHaveBeenCalledWith(emittedComment);
+  });
+
+  it('should not submit if the form is invalid', async () => {
+    component.commentForm.get('comment')?.setValue('');
+    fixture.detectChanges();
+
+    spyOn(component.onComment, 'emit');
+
+    await component.onSubmit();
+
+    expect(component.onComment.emit).not.toHaveBeenCalled();
+  });
+
+  it('should not submit if the projectRequestId is not set', async () => {
+    component.projectRequestId = undefined;
+    fixture.detectChanges();
+
+    spyOn(component.onComment, 'emit');
+
+    await component.onSubmit();
+
+    expect(component.onComment.emit).not.toHaveBeenCalled();
+  });
+
+  it('should clear the form after successful submit', async () => {
+    const comment = 'Test comment';
+    const emittedComment = { id: 1, comment };
+    projectRequestService.comment.and.returnValue(of(emittedComment));
+
+    component.commentForm.get('comment')?.setValue(comment);
+    fixture.detectChanges();
+
+    await component.onSubmit();
+
+    expect(component.commentForm.get('comment')?.value).toBe('');
   });
 });
--- frontend/src/app/components/navigation/navigation.component.spec.ts
+++ frontend/src/app/components/navigation/navigation.component.spec.ts
@@ -1,25 +1,73 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthentificationService } from 'src/app/services/authentification-service/authentification.service';
 import { NavigationComponent } from './navigation.component';
 
-describe('NavigationComponent', () => {
+fdescribe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
+  let authService: jasmine.SpyObj<AuthentificationService>;
 
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('AuthentificationService', ['getUserRoles', 'logout', 'isAuthenticated']);
+
     await TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
-    })
-    .compileComponents();
+      declarations: [NavigationComponent],
+      imports: [RouterTestingModule],
+      providers: [{ provide: AuthentificationService, useValue: spy }],
+    }).compileComponents();
+
+    authService = TestBed.inject(
+      AuthentificationService
+    ) as jasmine.SpyObj<AuthentificationService>;
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
+    authService.getUserRoles.and.returnValue([]);
+    authService.isAuthenticated.and.returnValue(false); // Add this line
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call generateLinks on ngOnInit', () => {
+    spyOn(component, 'generateLinks');
+
+    component.ngOnInit();
+
+    expect(component.generateLinks).toHaveBeenCalled();
+  });
+
+  it('should generate mainLinks with 3 items', () => {
+      component.generateLinks();
+      expect(component.mainLinks.length).toBe(3);
+    });
+
+  it('should generate protectedLinks with 1 item if user has "admin" role', () => {
+    authService.getUserRoles.and.returnValue(['admin']);
+    
+    component.ngOnInit(); 
+    component.generateLinks();
+  
+    console.log('User Roles:', component.userRoles);
+    console.log('Protected Links:', component.protectedLinks);
+  
+    expect(component.protectedLinks.length).toBe(1);
+  });
+  
+  it('should generate protectedLinks with 0 items if user does not have "admin" role', () => {
+    authService.getUserRoles.and.returnValue(['user']);
+    component.generateLinks();
+    expect(component.protectedLinks.length).toBe(0);
+  });
+
+  it('should call authService.logout when logout is called', () => {
+    component.logout();
+
+    expect(authService.logout).toHaveBeenCalled();
   });
 });
